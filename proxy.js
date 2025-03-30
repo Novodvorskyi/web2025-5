2e10c8214d42ae1bc621e5aab128e33fcda35ab1
Роман Новодворський web2025-3 <novodvorskyiroma@gmail.com>
2025-03-30 15:38:29 +0300

    Додано реалізацію PUT-запиту для збереження зображень

--- proxy.js
+++ proxy.js
@@ -1,19 +1,38 @@
-const http = require('http');
-const fs = require('fs').promises;
-const { program } = require('commander');
-const path = require('path');
+const http = require("http");
+const { program } = require("commander");
+const fs = require("fs").promises;
+const path = require("path");
 
 program
-  .requiredOption('-h, --host <host>', 'Адреса сервера')
-  .requiredOption('-p, --port <port>', 'Порт сервера', parseInt)
-  .requiredOption('-c, --cache <path>', 'Шлях до директорії кешу');
+  .requiredOption("-h, --host <host>", "адреса сервера")
+  .requiredOption("-p, --port <port>", "порт сервера")
+  .requiredOption("-c, --cache <path>", "шлях до кешу");
 
 program.parse(process.argv);
 const options = program.opts();
+const cacheDir = options.cache;
 
 const server = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Проксі-сервер працює!');
+  if (req.method === "PUT") {
+    const code = req.url.slice(1); // Видаляємо "/"
+    const filePath = path.join(cacheDir, `${code}.jpg`);
+
+    let data = [];
+    req.on("data", (chunk) => data.push(chunk));
+    req.on("end", async () => {
+      try {
+        await fs.writeFile(filePath, Buffer.concat(data));
+        res.writeHead(201, { "Content-Type": "text/plain" });
+        res.end("Файл збережено!");
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Помилка збереження файлу!");
+      }
+    });
+  } else {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("Метод не дозволено!");
+  }
 });
 
 server.listen(options.port, options.host, () => {
